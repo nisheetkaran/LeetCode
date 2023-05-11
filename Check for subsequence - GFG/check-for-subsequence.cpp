26a6860d60c9aee9c5a4465b2d5ef017c9036191
nisheetkaran <77787531+nisheetkaran@users.noreply.github.com>
2023-05-11 19:23:38 +0530

    Added solution - LeetHub

---
+++ Check for subsequence - GFG/check-for-subsequence.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isSubSequence(string A, string B) 
+    {
+        int n = A.size();
+        int m = B.size();
+
+        int i = 0;
+        int j = 0;
+        
+        while(j < m)
+        {
+            if(A[i] == B[j]) i++;
+            j++;
+        }
+        
+        if(i == n) return 1;
+        else return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;  
+        Solution ob;
+        if(ob.isSubSequence(A,B))
+            cout<<"1"<<endl;
+        else
+            cout<<"0"<<endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
