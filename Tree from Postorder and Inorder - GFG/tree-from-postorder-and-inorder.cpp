325a4f03b96f9de20a4c6bd2870ff6a36e4a4123
nisheetkaran <77787531+nisheetkaran@users.noreply.github.com>
2023-06-21 03:13:08 +0530

    Added solution - LeetHub

---
+++ Tree from Postorder and Inorder - GFG/tree-from-postorder-and-inorder.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+//Function to return a tree created from postorder and inoreder traversals.
+Node* helper(int in[], int post[], int inStart, int inEnd, int postStart, int postEnd)     
+{
+        if (inStart > inEnd) {
+            return NULL;
+        }
+        
+        int rootVal = post[postEnd];
+        Node* root = new Node(rootVal);
+        
+        int rootIndex = inStart;
+        for (; rootIndex <= inEnd; rootIndex++) {
+            if (in[rootIndex] == rootVal) {
+                break;
+            }
+        }
+        
+        int leftTreeSize = rootIndex - inStart;
+        int rightTreeSize = inEnd - rootIndex;
+        
+        root->left = helper(in, post, inStart, rootIndex - 1, postStart, postStart + leftTreeSize - 1);
+        root->right = helper(in, post, rootIndex + 1, inEnd, postEnd - rightTreeSize, postEnd - 1);
+        
+        return root;
+}
+
+
+Node *buildTree(int in[], int post[], int n) {
+     
+     return helper(in, post, 0, n - 1, 0, n - 1);
+}
+
+
+
