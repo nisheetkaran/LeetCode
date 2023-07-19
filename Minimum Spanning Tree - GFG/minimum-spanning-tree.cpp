54a3e6fa62f4682588eb3fbd348fd7e12e617d9b
nisheetkaran <77787531+nisheetkaran@users.noreply.github.com>
2023-07-19 18:48:44 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 // } Driver Code Ends
 
-
 class DisjointSet {
-    vector<int> rank, parent, size;
+    
+    vector<int> rank, parent, size;   
 public:
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
@@ -54,43 +54,41 @@ public:
         }
     }
 };
+
 class Solution
 {
-public:
-    //Function to find sum of weights of edges of the Minimum Spanning Tree.
+	public:
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        // 1 - 2 wt = 5
-        /// 1 - > (2, 5)
-        // 2 -> (1, 5)
-
-        // 5, 1, 2
-        // 5, 2, 1
         vector<pair<int, pair<int, int>>> edges;
-        for (int i = 0; i < V; i++) {
-            for (auto it : adj[i]) {
-                int adjNode = it[0];
-                int wt = it[1];
-                int node = i;
-
-                edges.push_back({wt, {node, adjNode}});
+        for(int i = 0; i < V; i++) {
+            for(auto x : adj[i]){
+                int v = x[0];
+                int wt = x[1];
+                int u = i;
+                
+                
+                edges.push_back({wt, {u, v}});
             }
         }
+        
         DisjointSet ds(V);
         sort(edges.begin(), edges.end());
-        int mstWt = 0;
-        for (auto it : edges) {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-
+        int ans = 0;
+        
+        for (auto x : edges) {
+            
+            int wt = x.first;
+            int u = x.second.first;
+            int v = x.second.second;
+            
             if (ds.findUPar(u) != ds.findUPar(v)) {
-                mstWt += wt;
+                ans += wt;
                 ds.unionBySize(u, v);
             }
         }
-
-        return mstWt;
+        
+        return ans;
     }
 };
 
